a4bcc13056f74962072bfe758e684e534174f38f
unknown <miguelangeldelaguila@gmail.com>
2015-09-19 17:16:51 +0200

    update3.0

--- config/express.js
+++ config/express.js
@@ -36,12 +36,12 @@ module.exports = function(app) {
   });
   
   app.use(passport.initialize());
-  /*if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+  if ('production' === env) {
+    //app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
-  }*/
+  }
 
   if ('development' === env || 'test' === env) {
     app.use(require('connect-livereload')());
